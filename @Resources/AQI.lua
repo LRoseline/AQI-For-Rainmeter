780725aa3462f9c5e5e5775f0c59382d5d1f874b
LRoseline <amb17@naver.com>
2025-05-09 00:46:04 +0900

    First Commit

---
+++ @Resources/AQI.lua
@@ -0,0 +1,17 @@
+function colorGrade(grade)
+    if grade == "1" then
+        return "00FFFF"
+    elseif grade == "2" then
+        return "00ff00"
+    elseif grade == "3" then
+        return "ff7e00"
+    elseif grade == "4" then
+        return "FF0000"
+    elseif grade == "5" then
+        return "8f3f97"
+    elseif grade == "6" then
+        return "7e0023"
+    else
+        return "FFFFFF"
+    end
+end
